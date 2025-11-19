0daca3e279d5958ed5faf11b886de818f7521466
Marco Aurelio de Souza Santos <202511064879@alunos.estacio.br>
2025-11-19 14:12:17 +0000

    Jogo CartasSuperTrunfo.c Iniciante concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -40,7 +40,47 @@ int main() {
     printf("Código da carta: %s\n",Codigo);
     printf("Nome da cidade: %s\nPopulação: %i\n", Nome, Populacao);
     printf("Área: %.2fkm²\nPIB: %.2lf Bilhões de reais\n", Area, PIB);
-    printf("Número de pontos turísticos: %i\n", Numero);
+    printf("Número de pontos turísticos: %i\n\n", Numero);
+
+    // VARIÁVEIS CARTA 2
+    char Estado2[20];
+    char Codigo2[10];
+    char Nome2[20];
+    int Populacao2;
+    float Area2;
+    double PIB2;
+    int Numero2;
+
+    // LEITURA CARTA 2
+    printf("Digite seu estado: \n");
+    scanf(" %s",Estado2);
+    
+
+    printf("Digite o codigo da carta: \n");
+    scanf(" %s",Codigo2);
+    
+    printf("Digite o Nome da cidade: \n");
+    scanf(" %s",Nome2);
+    
+    printf("Digite a População: \n");
+    scanf("%i",&Populacao2);
+    
+    printf("Digite a Área em km²: \n");
+    scanf("%f", &Area2);
+
+    printf("Digite o PIB: \n");
+    scanf("%lf", &PIB2);
+    
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%i", &Numero2);
+
+    // IMPRIME CARTA 2
+
+    printf("Estado: %s\n", Estado2);
+    printf("Código da carta: %s\n",Codigo2);
+    printf("Nome da cidade: %s\nPopulação: %i\n", Nome2, Populacao2);
+    printf("Área: %.2fkm²\nPIB: %.2lf Bilhões de reais\n", Area2, PIB2);
+    printf("Número de pontos turísticos: %i\n", Numero2);
 
     return 0;
 
